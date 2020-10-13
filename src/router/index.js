a93af57eef61414855c38870d9d1fe7de8cd65e5
Cyril Lamirand @DEV <Contact.cyril1@gmail.com>
2020-10-13 17:54:53 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
 import MedicalMonitoring from "@/views/MedicalMonitoring"
 import Calendar from "@/views/Calendar"
 import Weather from "@/views/Weather"
