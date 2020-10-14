499b6b6a49fa429d7ba3fa95af920144dc030116
Cyril Lamirand @DEV <Contact.cyril1@gmail.com>
2020-10-14 18:08:45 +0200

    Updating Calendar

--- src/components/CalendarComponent.vue
+++ src/components/CalendarComponent.vue
@@ -4,18 +4,298 @@
       <H2>Calendrier - Octobre</H2>
     </div>
     <div class="container-fluid">
-      <calendar-view
-          :show-date="showDate"
-          class="theme-default holiday-us-traditional holiday-us-official">
-        <calendar-view-header
-            slot="header"
-            slot-scope="t"
-            :header-props="t.headerProps"
-            startingDayOfWeek="1"
-            @input="setShowDate" />
-            @click-date([date, calendarItems, windowEvent])
-      </calendar-view>
+      <!-- Semaine 1 -->
+      <div class="row" style="margin-top: 20px;">
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>27 Dimanche</h3>
+            <hr>
 
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>28 Lundi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>29 Mardi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>30 Mercredi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>01 Jeudi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>02 Vendredi</h3>
+            <hr>
+
+          </div>
+        </div>
+      </div>
+
+      <!-- Semaine 2 -->
+      <div class="row" style="margin-top: 20px;">
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>03 Samedi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>04 Dimanche</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>05 Lundi</h3>
+            <hr>
+            <p><b>9h45</b> - RDV Dr Artur</p>
+            <p>Médecin Généraliste</p>
+            <p>Début du traitement</p>
+            <p><router-link to="Medical">Voir : Ordonnance 54#205</router-link></p>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>06 Mardi</h3>
+            <hr>
+            <p>2ème jour traitement</p>
+            <p>Ordonnance 54#205</p>
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>07 Mercredi</h3>
+            <hr>
+            <p>3ème jour de traitement</p>
+            <p>Ordonnance 54#205</p>
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>08 Jeudi</h3>
+            <hr>
+            <p>4ème jour de traitement</p>
+            <p>Ordonnance 54#205</p>
+          </div>
+        </div>
+      </div>
+
+      <!-- Semaine 3 -->
+      <div class="row" style="margin-top: 20px;">
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>09 Vendredi</h3>
+            <hr>
+            <p>5ème jour de traitement</p>
+            <p>Ordonnance 54#205</p>
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>10 Samedi</h3>
+            <hr>
+            <p>6ème jour de traitement</p>
+            <p>Ordonnance 54#205</p>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>11 Dimanche</h3>
+            <hr>
+            <p>7ème jour de traitement</p>
+            <p>Ordonnance 54#205</p>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>12 Lundi</h3>
+            <hr>
+            <p>Fin du traitement</p>
+            <p><router-link to="Medical">Voir : Ordonnance 54#205</router-link></p>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>13 Mardi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>14 Mercredi</h3>
+            <hr>
+
+          </div>
+        </div>
+      </div>
+
+      <!-- Semaine 4 -->
+      <div class="row" style="margin-top: 20px;">
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>15 Jeudi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>16 Vendredi</h3>
+            <hr>
+            <p><b>15h15</b> - RDV Dr Artur</p>
+            <p>Contre-Visite</p>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>17 Samedi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>18 Dimanche</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>19 Lundi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>20 Mardi</h3>
+            <hr>
+
+          </div>
+        </div>
+      </div>
+
+      <!-- Semaine 5 -->
+      <div class="row" style="margin-top: 20px;">
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>21 Mercredi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>22 Jeudi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>23 Vendredi</h3>
+            <hr>
+            <p><b>10h05</b>RDV Dr MEROT</p>
+            <p>Cabinet Dentaire</p>
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>24 Samedi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>25 Dimanche</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>26 Lundi</h3>
+            <hr>
+
+          </div>
+        </div>
+      </div>
+
+      <!-- Semaine 1 -->
+      <div class="row" style="margin-top: 20px;">
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>27 Mardi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>28 Mercredi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>29 Jeudi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>30 Vendredi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>31 Samedi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="container-item-calendar">
+            <h3>01 Dimanche</h3>
+            <hr>
+
+          </div>
+        </div>
+      </div>
 
     </div>
     <!-- Navigation Fixe : BAS -->
@@ -25,31 +305,18 @@
 
 <script>
 import Navbar from "@/views/Navbar";
-import { CalendarView, CalendarViewHeader } from "vue-simple-calendar"
-// The next two lines are processed by webpack. If you're using the component without webpack compilation,
-// you should just create <link> elements for these. Both are optional, you can create your own theme if you prefer.
-require("vue-simple-calendar/static/css/default.css")
-require("vue-simple-calendar/static/css/holidays-us.css")
 
 export default {
 name: "CalendarComponent",
-  data: function() {
-    return { showDate: new Date() }
-  },
   components: {
-    CalendarView,
-    CalendarViewHeader,
     Navbar
-  },
-  methods: {
-    setShowDate(d) {
-      this.showDate = d;
-    },
   }
 }
 </script>
 
 <style scoped>
+
+.container-item-calendar{ height: 160px; background-color: rgba(12,12,12,0.8); border-radius: 8px; padding-top: 10px; padding-left: 10px; padding-right: 10px; }
 
 #CalendarComponent{}
 
@@ -64,21 +331,10 @@ name: "CalendarComponent",
   text-transform: uppercase;
 }
 
-.div-14{ width: 14.28%; height: 250px; padding: 5px; margin-top:20px; }
+h3{ color: #F5F5F5; font-size: 18px; }
 
-.contentCalendar{ background-color: rgba(12,12,12,0.8);; height: 100%; padding-top: 15px; padding-left: 10px; padding-right: 10px; border-radius: 8px; }
-
-h3{ color: #F5F5F5; font-size: 22px; }
+p{ font-size: 12px; color: #F5F5F5; }
 
 hr{ border:1px solid white; }
-
-.c-grey{ background-color: #4e555b !important; }
-
-.c-end{ background-color: #2c3e50 !important; }
-
-.c-now{ background-color: #0f6674 !important; }
-
-ul, li, p{ color: #F5F5F5; }
-
 
 </style>
