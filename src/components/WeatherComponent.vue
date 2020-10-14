3ba1a913d4f1d1cbfec5f7eb3c9f31e0bbf5da26
Cyril Lamirand @DEV <Contact.cyril1@gmail.com>
2020-10-14 18:15:27 +0200

    Updating WeatherComponent

--- src/components/WeatherComponent.vue
+++ src/components/WeatherComponent.vue
@@ -1,6 +1,60 @@
 <template>
   <div id="WeatherComponent">
-    <h2>Weather Component</h2>
+
+    <div class="WeatherContainerContent">
+      <vue-weather
+          api-key="a9a79e59327a0dc88a110c7f442c8db8"
+          units="uk"
+          language="fr"
+          bar-color="#F5F5F5"
+          text-color="#F5F5F5"
+          hide-header="true"
+      />
+    </div>
+
+    <div class="WeatherContainerContent">
+      <vue-weather
+          api-key="a9a79e59327a0dc88a110c7f442c8db8"
+          units="uk"
+          language="fr"
+          bar-color="#F5F5F5"
+          text-color="#F5F5F5"
+          hide-header="true"
+      />
+    </div>
+
+    <div class="WeatherContainerContent">
+      <vue-weather
+          api-key="a9a79e59327a0dc88a110c7f442c8db8"
+          units="uk"
+          language="fr"
+          bar-color="#F5F5F5"
+          text-color="#F5F5F5"
+          hide-header="true"
+      />
+    </div>
+
+    <div class="WeatherContainerContent">
+      <vue-weather
+          api-key="a9a79e59327a0dc88a110c7f442c8db8"
+          units="uk"
+          language="fr"
+          bar-color="#F5F5F5"
+          text-color="#F5F5F5"
+          hide-header="true"
+      />
+    </div>
+
+    <div class="WeatherContainerContent">
+      <vue-weather
+          api-key="a9a79e59327a0dc88a110c7f442c8db8"
+          units="uk"
+          language="fr"
+          bar-color="#F5F5F5"
+          text-color="#F5F5F5"
+          hide-header="true"
+      />
+    </div>
     <!-- Navigation Fixe : BAS -->
     <Navbar/>
   </div>
@@ -8,14 +62,29 @@
 
 <script>
 import Navbar from "@/views/Navbar";
+import VueWeather from "vue-weather-widget";
+
 export default {
 name: "WeatherComponent",
-  components: {Navbar}
+  components: {
+    VueWeather,
+    Navbar
+  }
 }
 </script>
 
 <style scoped>
 
 #WeatherComponent{}
+
+.WeatherContainerContent{
+  display: flex;
+  justify-content: center;
+  background-color: rgba(12,12,12,0.8);
+  border-radius: 8px;
+  padding-top: 20px;
+  padding-bottom: 15px;
+  margin-top: 30px;
+}
 
 </style>
