a859d1450dd24bda285333de3436e3c5b39502b6
Cyril Lamirand @DEV <Contact.cyril1@gmail.com>
2020-10-14 15:54:20 +0200

    Push Calendar

--- src/components/CalendarComponent.vue
+++ src/components/CalendarComponent.vue
@@ -1,6 +1,300 @@
 <template>
   <div id="CalendarComponent">
-    <H2>Calendar Component</H2>
+    <div class="col-12" id="HeaderCalendar">
+      <H2>Calendrier - Octobre</H2>
+    </div>
+    <div class="container-fluid">
+
+      <!-- Semaine 1 -->
+
+      <div class="row">
+        <div class="div-14">
+          <div class="contentCalendar c-grey">
+            <h3>27 Dimanche</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar c-grey">
+            <h3>28 Lundi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar c-grey">
+            <h3>29 Mardi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar c-grey">
+            <h3>30 Mercredi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar c-end">
+            <h3>01 Jeudi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar c-end">
+            <h3>02 Vendredi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar c-end">
+            <h3>03 Samedi</h3>
+            <hr>
+
+          </div>
+        </div>
+      </div>
+
+      <!-- Semaine 2 -->
+
+      <div class="row">
+        <div class="div-14">
+          <div class="contentCalendar c-end">
+            <h3>04 Dimanche</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar c-end">
+            <h3>05 Lundi</h3>
+            <hr>
+            <p>10h30 - RDV Docteur ARTUR</p>
+            <p>Médecin Généraliste</p>
+            <p>Sous traitement</p>
+            <p>Ordonnance #4500852</p>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar c-end">
+            <h3>06 Mardi</h3>
+            <hr>
+            <p>Sous traitement</p>
+            <p>Ordonnance #4500852</p>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar c-end">
+            <h3>07 Mercredi</h3>
+            <hr>
+            <p>Sous traitement</p>
+            <p>Ordonnance #4500852</p>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar c-end">
+            <h3>08 Jeudi</h3>
+            <hr>
+            <p>Sous traitement</p>
+            <p>Ordonnance #4500852</p>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar c-end">
+            <h3>09 Vendredi</h3>
+            <hr>
+            <p>Sous traitement</p>
+            <p>Ordonnance #4500852</p>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar c-end">
+            <h3>10 Samedi</h3>
+            <hr>
+
+          </div>
+        </div>
+      </div>
+
+      <!-- Semaine 3 -->
+
+      <div class="row">
+        <div class="div-14">
+          <div class="contentCalendar c-end">
+            <h3>11 Dimanche</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar c-end">
+            <h3>12 Lundi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar c-end">
+            <h3>13 Mardi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar c-now">
+            <h3>14 Mercredi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar">
+            <h3>15 Jeudi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar">
+            <h3>16 Vendredi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar">
+            <h3>17 Samedi</h3>
+            <hr>
+
+          </div>
+        </div>
+      </div>
+
+      <!-- Semaine 4 -->
+
+      <div class="row">
+        <div class="div-14">
+          <div class="contentCalendar">
+            <h3>18 Dimanche</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar">
+            <h3>19 Lundi</h3>
+            <hr>
+
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar">
+            <h3>20 Mardi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar">
+            <h3>21 Mercredi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar">
+            <h3>22 Jeudi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar">
+            <h3>23 Vendredi</h3>
+            <hr>
+            <p>14h45 - RDV Docteur ARTUR</p>
+            <p>Médecin Généraliste</p>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar">
+            <h3>24 Samedi</h3>
+            <hr>
+
+          </div>
+        </div>
+      </div>
+
+      <!-- Semaine 5 -->
+
+      <div class="row">
+        <div class="div-14">
+          <div class="contentCalendar">
+            <h3>25 Dimanche</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar">
+            <h3>26 Lundi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar">
+            <h3>27 Mardi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar">
+            <h3>28 Mercredi</h3>
+            <hr>
+            <p>9h30 - RDV Docteur GUERRIN</p>
+            <p>Dentiste</p>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar">
+            <h3>29 Jeudi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar">
+            <h3>30 Vendredi</h3>
+            <hr>
+
+          </div>
+        </div>
+        <div class="div-14">
+          <div class="contentCalendar">
+            <h3>31 Samedi</h3>
+            <hr>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+
 
     <!-- Navigation Fixe : BAS -->
     <Navbar/>
@@ -19,5 +313,33 @@ name: "CalendarComponent",
 <style scoped>
 
 #CalendarComponent{}
+
+#HeaderCalendar{
+  width: 100%;
+  height: 80px;
+  background-color: rgba(12,12,12,0.8);
+  display: flex;
+  justify-content: center;
+  color: #F5F5F5;
+  padding-top:20px;
+  text-transform: uppercase;
+}
+
+.div-14{ width: 14.28%; height: 250px; padding: 5px; margin-top:20px; }
+
+.contentCalendar{ background-color: rgba(12,12,12,0.8);; height: 100%; padding-top: 15px; padding-left: 10px; padding-right: 10px; border-radius: 8px; }
+
+h3{ color: #F5F5F5; }
+
+hr{ border:1px solid white; }
+
+.c-grey{ background-color: #4e555b !important; }
+
+.c-end{ background-color: #2c3e50 !important; }
+
+.c-now{ background-color: #0f6674 !important; }
+
+ul, li, p{ color: #F5F5F5; }
+
 
 </style>
