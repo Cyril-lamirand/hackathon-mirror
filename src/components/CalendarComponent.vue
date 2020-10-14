8a3de8c1fb5cf2571dfa358a59b16266e65a73bb
Cyril Lamirand @DEV <Contact.cyril1@gmail.com>
2020-10-14 16:48:15 +0200

    Pushing

--- src/components/CalendarComponent.vue
+++ src/components/CalendarComponent.vue
@@ -4,298 +4,20 @@
       <H2>Calendrier - Octobre</H2>
     </div>
     <div class="container-fluid">
+      <calendar-view
+          :show-date="showDate"
+          class="theme-default holiday-us-traditional holiday-us-official">
+        <calendar-view-header
+            slot="header"
+            slot-scope="t"
+            :header-props="t.headerProps"
+            startingDayOfWeek="1"
+            @input="setShowDate" />
+            @click-date([date, calendarItems, windowEvent])
+      </calendar-view>
 
-      <!-- Semaine 1 -->
-
-      <div class="row">
-        <div class="div-14">
-          <div class="contentCalendar c-grey">
-            <h3>27 Dimanche</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar c-grey">
-            <h3>28 Lundi</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar c-grey">
-            <h3>29 Mardi</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar c-grey">
-            <h3>30 Mercredi</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar c-end">
-            <h3>01 Jeudi</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar c-end">
-            <h3>02 Vendredi</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar c-end">
-            <h3>03 Samedi</h3>
-            <hr>
-
-          </div>
-        </div>
-      </div>
-
-      <!-- Semaine 2 -->
-
-      <div class="row">
-        <div class="div-14">
-          <div class="contentCalendar c-end">
-            <h3>04 Dimanche</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar c-end">
-            <h3>05 Lundi</h3>
-            <hr>
-            <p>10h30 - RDV Docteur ARTUR</p>
-            <p>Médecin Généraliste</p>
-            <p>Sous traitement</p>
-            <p>Ordonnance #4500852</p>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar c-end">
-            <h3>06 Mardi</h3>
-            <hr>
-            <p>Sous traitement</p>
-            <p>Ordonnance #4500852</p>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar c-end">
-            <h3>07 Mercredi</h3>
-            <hr>
-            <p>Sous traitement</p>
-            <p>Ordonnance #4500852</p>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar c-end">
-            <h3>08 Jeudi</h3>
-            <hr>
-            <p>Sous traitement</p>
-            <p>Ordonnance #4500852</p>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar c-end">
-            <h3>09 Vendredi</h3>
-            <hr>
-            <p>Sous traitement</p>
-            <p>Ordonnance #4500852</p>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar c-end">
-            <h3>10 Samedi</h3>
-            <hr>
-
-          </div>
-        </div>
-      </div>
-
-      <!-- Semaine 3 -->
-
-      <div class="row">
-        <div class="div-14">
-          <div class="contentCalendar c-end">
-            <h3>11 Dimanche</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar c-end">
-            <h3>12 Lundi</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar c-end">
-            <h3>13 Mardi</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar c-now">
-            <h3>14 Mercredi</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar">
-            <h3>15 Jeudi</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar">
-            <h3>16 Vendredi</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar">
-            <h3>17 Samedi</h3>
-            <hr>
-
-          </div>
-        </div>
-      </div>
-
-      <!-- Semaine 4 -->
-
-      <div class="row">
-        <div class="div-14">
-          <div class="contentCalendar">
-            <h3>18 Dimanche</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar">
-            <h3>19 Lundi</h3>
-            <hr>
-
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar">
-            <h3>20 Mardi</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar">
-            <h3>21 Mercredi</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar">
-            <h3>22 Jeudi</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar">
-            <h3>23 Vendredi</h3>
-            <hr>
-            <p>14h45 - RDV Docteur ARTUR</p>
-            <p>Médecin Généraliste</p>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar">
-            <h3>24 Samedi</h3>
-            <hr>
-
-          </div>
-        </div>
-      </div>
-
-      <!-- Semaine 5 -->
-
-      <div class="row">
-        <div class="div-14">
-          <div class="contentCalendar">
-            <h3>25 Dimanche</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar">
-            <h3>26 Lundi</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar">
-            <h3>27 Mardi</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar">
-            <h3>28 Mercredi</h3>
-            <hr>
-            <p>9h30 - RDV Docteur GUERRIN</p>
-            <p>Dentiste</p>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar">
-            <h3>29 Jeudi</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar">
-            <h3>30 Vendredi</h3>
-            <hr>
-
-          </div>
-        </div>
-        <div class="div-14">
-          <div class="contentCalendar">
-            <h3>31 Samedi</h3>
-            <hr>
-
-          </div>
-        </div>
-      </div>
 
     </div>
-
-
-
     <!-- Navigation Fixe : BAS -->
     <Navbar/>
   </div>
@@ -303,10 +25,27 @@
 
 <script>
 import Navbar from "@/views/Navbar";
+import { CalendarView, CalendarViewHeader } from "vue-simple-calendar"
+// The next two lines are processed by webpack. If you're using the component without webpack compilation,
+// you should just create <link> elements for these. Both are optional, you can create your own theme if you prefer.
+require("vue-simple-calendar/static/css/default.css")
+require("vue-simple-calendar/static/css/holidays-us.css")
 
 export default {
 name: "CalendarComponent",
-  components: {Navbar}
+  data: function() {
+    return { showDate: new Date() }
+  },
+  components: {
+    CalendarView,
+    CalendarViewHeader,
+    Navbar
+  },
+  methods: {
+    setShowDate(d) {
+      this.showDate = d;
+    },
+  }
 }
 </script>
 
